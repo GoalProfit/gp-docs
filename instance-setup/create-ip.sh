26b3e73d1383dd2120de15b68eed03a14791b89f
Sergey Rozhkov <sergey.rozhkov@goalprofit.com>
2023-08-15 18:59:57 +0200

    removed non-public information

--- instance-setup/create-ip.sh
+++
@@ -1,3 +0,0 @@
-INSTANCE_NAME=$1
-REGION=$2
-gcloud compute addresses create ${INSTANCE_NAME} --project=mindful-phalanx-313619 --region=${REGION}
--- instance-setup/create-merkal.sh
+++
@@ -1,1 +0,0 @@
-bash create-vm.sh merkal n2-highmem-8 europe-southwest1 europe-southwest1-a 1000
